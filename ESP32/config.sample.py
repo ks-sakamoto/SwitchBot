5f73c1ee4af8ffd6a53aec1ccce9f6a507a754df
ks-sakamoto <ks.netgit@gmail.com>
2024-12-14 18:43:40 +0900

    BLEの動作確認スクリプトを追加。それに伴ってconfig.pyの修正

--- ESP32/config.sample.py
+++ ESP32/config.sample.py
@@ -1,3 +1,3 @@
 # config.sample.py
 SERVICE_UUID = "your-service-uuid-here"
-CHARACTERISTIC_UUID = "your-characteristic-uuid-here"
+CHAR_UUID = "your-characteristic-uuid-here"
--- ESP32/tests/test_servo.py
+++ ESP32/tests/test_servo.py
@@ -17,25 +17,27 @@ class Servo:
         # 2.4ms（180度） = 12% duty
         self.min_duty = 26  # 0.5ms/20ms * 1024
         self.max_duty = 123  # 2.4ms/20ms * 1024
-        self.defalut_duty = 100  # 初期角度duty
+        self.defalut_duty = 103  # 初期角度duty
 
         # サーボモータの動作音を止めるため(これによって角度は変わらない)
         time.sleep(1)
         self.servo.duty(0)
 
-    def set_angle(self, angle) -> None:
+    def set_angle(self, duty) -> None:
         """
         角度を設定し、サーボモータを動作させる。
         動作後は初期角度に戻す。
 
         Parameters
         ----------
-        angle : fload
-            サーボモータを動作させる角度
+        # angle : fload
+        #     サーボモータを動作させる角度
+        duty : int
+            サーボモータを動作させるデューティー比（本来はangleにしたい）
         """
-        duty = int(
-            self.min_duty + (self.max_duty - self.min_duty) * angle / 180
-        )
+        # duty = int(
+        #     self.min_duty + (self.max_duty - self.min_duty) * angle / 180
+        # )
         time.sleep(2)
         self.servo.duty(duty)
 
@@ -61,7 +63,7 @@ def main():
     for _ in range(3):
         time.sleep(1)
         print("start servo")
-        servo.set_angle(90)
+        servo.set_angle(113)
         servo.set_default_angle()
 
     servo.stop()
