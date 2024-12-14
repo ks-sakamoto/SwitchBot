75f823cde5a7be8a82b9f3e897a9704ed5d4b75f
ks-sakamoto <ks.netgit@gmail.com>
2024-12-14 18:44:46 +0900

    modify test script for main.py. Add BLE movement

--- ESP32/tests/main.py
+++ ESP32/tests/main.py
@@ -1,29 +1,16 @@
 """
 電池駆動の確認をするためのテストスクリプト
-- 内臓LEDのLチカ
-- サーボモータの起動
+- BLE起動
+  - test_bluetooth内でLチカとサーボモータのテストを実行可能
 - deepsleepの開始
 """
 
-import time
-
+import test_bluetooth
 import test_deepsleep
-import test_servo
-from machine import Pin
 
 if __name__ == "__main__":
-    # 内臓LEDピンは2番
-    pin = Pin(2, Pin.OUT)
-
-    # Lチカ3回
-    for _ in range(3):
-        time.sleep(1)
-        pin.on()  # LEDオン
-        time.sleep(1)
-        pin.off()  # LEDオフ
-
-    # サーボモータ起動
-    test_servo.main()
+    # BLE起動
+    test_bluetooth.main()
 
     # deepsleep開始
     test_deepsleep.main()
