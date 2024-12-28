f0bcd2b06e97805c2d57e5d6cc2f05eda6856ea3
ks-sakamoto <ks.netgit@gmail.com>
2024-12-28 20:37:00 +0900

    Add main script

---
+++ ESP32/deepsleep_module.py
@@ -0,0 +1,28 @@
+import machine
+from machine import Pin
+
+import esp32
+
+
+class DeepSleep:
+    def __init__(self, pin) -> None:
+        # ウェイクアップピンの設定
+        self.wake_pin = Pin(pin, mode=Pin.IN, pull=Pin.PULL_UP)
+        # ウェイクアップ要因の設定
+        esp32.wake_on_ext0(pin=self.wake_pin, level=esp32.WAKEUP_ALL_LOW)
+
+    def start_deepsleep(self) -> None:
+        """DeepSleepモードの開始"""
+        print("Going to deep sleep...")
+        machine.deepsleep()
+
+
+def main() -> None:
+    sleep_controller = DeepSleep(27)
+
+    # DeepSleepモードを開始
+    sleep_controller.start_deepsleep()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ESP32/servo_module.py
@@ -0,0 +1,68 @@
+import time
+
+from machine import PWM, Pin
+
+
+class Servo:
+    def __init__(self, pin) -> None:
+        # PWMの設定(周波数50Hz)
+        self.servo = PWM(Pin(pin), freq=50)
+        # デューティサイクルの範囲を設定
+        # 0.5ms（0度） = 2.5% duty
+        # 2.4ms（180度） = 12% duty
+        self.min_duty = 26  # 0.5ms/20ms * 1024
+        self.max_duty = 123  # 2.4ms/20ms * 1024
+        self.defalut_duty = 103  # 初期角度duty
+
+        # サーボモータの動作音を止めるため(これによって角度は変わらない)
+        time.sleep(1)
+        self.servo.duty(0)
+
+    def set_angle(self, duty) -> None:
+        """
+        角度を設定し、サーボモータを動作させる。
+        動作後は初期角度に戻す。
+
+        Parameters
+        ----------
+        angle : fload
+            サーボモータを動作させる角度
+        duty : int
+            サーボモータを動作させるデューティー比（本来はangleにしたい）
+        """
+        # duty = int(
+        #     self.min_duty + (self.max_duty - self.min_duty) * angle / 180
+        # )
+        time.sleep(2)
+        self.servo.duty(duty)
+
+    def set_default_angle(self) -> None:
+        """サーボモータを初期角度に戻す"""
+        time.sleep(2)
+        self.servo.duty(self.defalut_duty)
+        time.sleep(1)
+        self.servo.duty(0)
+
+    def stop(self) -> None:
+        time.sleep(1)
+        self.servo.duty(self.defalut_duty)
+        time.sleep(2)
+        self.servo.deinit()
+
+
+def main():
+    # GPIO15番ピンにサーボモータを接続
+    servo = Servo(26)
+
+    # サーボモータ始動
+    print("start servo")
+    servo.set_angle(115)
+    servo.set_default_angle()
+
+    # サーボモータ停止
+    servo.stop()
+    print("stop servo")
+
+
+if __name__ == "__main__":
+    main()
